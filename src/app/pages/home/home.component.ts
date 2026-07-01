eba7f7630ac8dadd9cf7c0a2c7e389104a6f0e13
valenrocca <136339671+valenroccaZB@users.noreply.github.com>
2026-07-01 13:58:18 -0300

    ultimos cambios v2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,39 +1,11 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { environment } from '../../../environments/environment';
-import { RegisterModalComponent } from '../../components/register-modal/register-modal.component';
+import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RegisterModalComponent, FooterComponent],
+  imports: [FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  showRegister = false;
-  registrationComplete = false;
-  private successCloseTimer: ReturnType<typeof setTimeout> | null = null;
-
-  ngOnInit(): void {
-    this.showRegister = environment.registrationGateEnabled;
-  }
-
-  ngOnDestroy(): void {
-    if (this.successCloseTimer !== null) {
-      clearTimeout(this.successCloseTimer);
-    }
-  }
-
-  openRegister(): void {
-    this.showRegister = true;
-  }
-
-  onRegistrationSuccess(): void {
-    this.registrationComplete = true;
-    this.successCloseTimer = setTimeout(() => {
-      this.showRegister = false;
-      this.successCloseTimer = null;
-    }, 1000);
-  }
-}
+export class HomeComponent {}
